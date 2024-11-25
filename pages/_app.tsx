c5fd7b557d27cf33accc43173781bba60410297d
Dieter Walckiers <dieter@manual.to>
2024-11-25 16:35:11 +0100

    mailchimp form embed

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -15,6 +15,7 @@ export default function App({ Component, pageProps }: AppProps) {
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
         <link rel="manifest" href="/site.webmanifest" />
         <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <link rel="stylesheet" href="//cdn-images.mailchimp.com/embedcode/classic-061523.css" type="text/css" />
         <meta name="color-scheme" content="light only" />
       </Head>
       <main className={raleway.className} style={{ backgroundColor: "white" }}>
