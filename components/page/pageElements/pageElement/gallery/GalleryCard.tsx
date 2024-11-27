a40d75b77ac77f95ac7acba8d5a3c6a21782a9b2
Dieter Walckiers <dieter@manual.to>
2024-11-27 19:51:11 +0100

    gallery

--- components/page/pageElements/pageElement/gallery/GalleryCard.tsx
+++ components/page/pageElements/pageElement/gallery/GalleryCard.tsx
@@ -1,16 +1,24 @@
+import {
+    Modal,
+    ModalOverlay,
+    ModalContent,
+    useDisclosure,
+    Box
+} from "@chakra-ui/react";
 import { Flex } from "@chakra-ui/react";
 import Image from "next/image";
-import { useNextSanityImage } from "next-sanity-image";
 import React from "react";
-
 import { client } from "../../../../../sanity/lib/client";
+import { useNextSanityImage } from "next-sanity-image";
 
 interface Props {
     imageData: any;
+    cardWidthMd: string;
 }
 
 const GalleryCard: React.FC<Props> = ({
     imageData,
+    cardWidthMd,
 }) => {
 
     const imageProps: Record<string, any> = useNextSanityImage(
@@ -18,43 +26,41 @@ const GalleryCard: React.FC<Props> = ({
         imageData,
     );
 
-    return (
-        // <GridItem>
-        //     <Image
-        //         {...imageProps as any}
-        //         style={{ width: "100%", height: 'auto' }}
-        //         sizes="(max-width: 48em) 100vw,
-        //                 600px"
-        //         placeholder="blur"
-        //         blurDataURL={imageData.asset.metadata.lqip}
-        //         alt="" // TODO add alt text to cms
-        //         // onClick={clickImg}
-        //     />
-        // </GridItem>
-        // <Link
-        //     href={`/books/${book.slug}`}
-        //     as={NextLink}
-        // >
-        <Flex
-            width={{ base: "100%", md: "250px" }}
-            direction="column"
-            justifyContent="center"
-            alignItems="center"
-            mx={{ base: 0, md: 6 }}
-            my={{ base: 0, md: 6 }}
-            cursor="pointer"
-        >
-            <Image
-                {...imageProps as any}
-                style={{ width: "100%", height: 'auto' }}
-                sizes="(max-width: 48em) 100vw, 300px"
-                placeholder="blur"
-                blurDataURL={imageData.asset.metadata.lqip}
-                alt="" // TODO add alt text to cms
-            />
-        </Flex>
-        // </Link>
+    const { isOpen, onOpen, onClose } = useDisclosure();
 
+    return (
+        <>
+            <Flex
+                width={{ base: "100%", md: cardWidthMd }}
+                direction="column"
+                justifyContent="center"
+                alignItems="center"
+                cursor="pointer"
+            >
+                <Image
+                    {...imageProps as any}
+                    style={{ width: "100%", height: 'auto' }}
+                    sizes="(max-width: 768px) 100vw, 50vw, 33vw"
+                    placeholder="blur"
+                    blurDataURL={imageData.asset.metadata.lqip}
+                    alt="" // TODO add alt text to cms
+                    onClick={() => onOpen()}
+                />
+            </Flex>
+            <Modal isOpen={isOpen} onClose={onClose} size="6xl" >
+                <ModalOverlay />
+                <ModalContent>
+                    <Image
+                        {...imageProps as any}
+                        style={{ width: "100%", height: 'auto' }}
+                        sizes="(max-width: 768px) 98vw, 1152px"
+                        placeholder="blur"
+                        blurDataURL={imageData.asset.metadata.lqip}
+                        alt="" // TODO add alt text to cms
+                    />
+                </ModalContent>
+            </Modal>
+        </>
     )
 }
 
--- components/page/pageElements/pageElement/gallery/index.tsx
+++ components/page/pageElements/pageElement/gallery/index.tsx
@@ -6,6 +6,7 @@ import * as React from "react";
 import { client } from "../../../../../sanity/lib/client";
 import { PageElementGallery } from "../../../../../shared/contract";
 import GalleryCard from "./GalleryCard";
+import { useMemo } from "react";
 
 interface IProps {
     element: PageElementGallery;
@@ -30,35 +31,31 @@ const PageElement: React.FC<IProps> = ({ element }) => {
 
     console.log("imageDatas", imageDatas)
 
+    const cardWidthMd = useMemo(() => {
+        if (imageDatas.length === 1) {
+            return "80%";
+        }
+        if (imageDatas.length === 2) {
+            return "48%";
+        }
+        return "32%";
+    }, [imageDatas.length]);
+
     return (
         <Box w="100%">
             <Flex
                 direction={{ base: "column", md: "row" }}
                 flexWrap="wrap"
-                justifyContent="center"
+                justifyContent={imageDatas.length === 1 ? "center": "space-between"}
+                className="flx"
             >
                 {imageDatas.map((imageData, index) => {
                     return (
                         <GalleryCard
                             key={`gallery${index}`}
                             imageData={imageData}
+                            cardWidthMd={cardWidthMd}
                         />
-                        //     <Image
-                        //         // key={`gallery${index}`}
-                        //         // src={asset.url}
-                        //         // alt=""
-                        //         // width={asset.width}
-                        //         // height={asset.height}
-                        //         {...imageProps as any}
-                        //         style={{ width: "100%", height: 'auto', cursor: element.link ? "pointer" : "default" }}
-                        //         sizes="(max-width: 48em) 100vw,
-                        //             600px"
-                        //         placeholder="blur"
-                        //         blurDataURL={imageData.asset.metadata.lqip}
-                        //         alt="" // TODO add alt text to cms
-                        //         onClick={clickImg}
-                        //     />
-                        // </GalleryGridItem>
                     )
                 })}
             </Flex>
